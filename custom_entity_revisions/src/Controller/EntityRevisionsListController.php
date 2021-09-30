2fb07aa291aceba8ba391537aafcdaba34100774
Patrick Schilf <pschilf@unb.ca>
2021-09-30 11:10:33 -0300

    Determine entity type from request

--- custom_entity_revisions/src/Controller/EntityRevisionsListController.php
+++ custom_entity_revisions/src/Controller/EntityRevisionsListController.php
@@ -3,6 +3,7 @@
 namespace Drupal\custom_entity_revisions\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Controller to respond entity revision routes.
@@ -15,11 +16,20 @@ class EntityRevisionsListController extends ControllerBase {
    * @return array
    *   A render array.
    */
-  public function listing() {
+  public function listing(Request $request) {
+    $params = array_filter(array_keys($request->attributes->all()), function ($param) {
+      return substr($param, 0, 1) !== '_';
+    });
+
+    $entity_type_id = $params[array_keys($params)[0]];
+    $entity = $request->get($entity_type_id);
+
     return [
       '#type' => 'html_tag',
       '#tag' => 'p',
-      '#value' => $this->t('Here be a listing of entity revisions.'),
+      '#value' => $this->t('Here be a listing of @entity revisions.', [
+        '@entity' => $entity->label(),
+      ]),
     ];
   }
 
