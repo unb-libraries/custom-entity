4e40b2ff5bb13b6196258fa9f26b88d7f5b26147
Patrick Schilf <pschilf@unb.ca>
2021-09-30 08:37:57 -0300

    Add revisionable entity storage handler

---
+++ custom_entity_revisions/src/Entity/Storage/RevisionableEntityStorageInterface.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\custom_entity_revisions\Entity\Storage;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\RevisionableStorageInterface;
+use Drupal\Core\Entity\RevisionableInterface;
+
+/**
+ * Storage handler interface for revisionable entities.
+ *
+ * @package Drupal\lib_unb_custom_entity\Entity\Storage
+ */
+interface RevisionableEntityStorageInterface extends RevisionableStorageInterface {
+
+  /**
+   * Load all revisions of the given entity.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity.
+   *
+   * @return \Drupal\Core\Entity\RevisionableInterface
+   *   An array of entity revisions.
+   */
+  public function loadEntityRevisions(EntityInterface $entity);
+
+  /**
+   * Load the revision that preceded the one given.
+   *
+   * @param \Drupal\Core\Entity\RevisionableInterface $entity_revision
+   *   A revision of an entity.
+   *
+   * @return \Drupal\Core\Entity\EntityInterface|false
+   *   A revision of an entity. FALSE if there is no previous revision, i.e.
+   *   if the given entity is the first revision.
+   */
+  public function loadPreviousRevision(RevisionableInterface $entity_revision);
+
+}
---
+++ custom_entity_revisions/src/Entity/Storage/RevisionableEntityStorageTrait.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\custom_entity_revisions\Entity\Storage;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\RevisionableInterface;
+
+/**
+ * Trait to enhance Drupal's default storage handler for revisionable entities.
+ */
+trait RevisionableEntityStorageTrait {
+
+  /**
+   * {@inheritDoc}
+   */
+  public function loadEntityRevisions(EntityInterface $entity) {
+    /** @var \Drupal\custom_entity_revisions\Entity\Storage\RevisionableEntityStorageInterface $this */
+    $database = \Drupal::database();
+
+    $revision_id_key = $this->getEntityType()->getKey('revision');
+    $revision_table = $this->getEntityType()->getRevisionTable();
+
+    $revision_ids = $database->query(
+      "SELECT {$revision_id_key} FROM {$revision_table} WHERE id = :entity_id", [
+        ':entity_id' => $entity->id(),
+      ]
+    )->fetchCol();
+
+    return $this->loadMultipleRevisions($revision_ids);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function loadPreviousRevision(RevisionableInterface $entity_revision) {
+    /** @var \Drupal\custom_entity_revisions\Entity\Storage\RevisionableEntityStorageInterface $this */
+    $database = \Drupal::database();
+
+    $entity_revision_key = $this->getEntityType()->getKey('revision');
+    $revision_table = $this->getEntityType()->getRevisionTable();
+
+    $params = [
+      ':entity_id' => $entity_revision->id(),
+      ':entity_revision_id' => $entity_revision->getRevisionId(),
+      ':entity_revision_key' => $entity_revision_key,
+    ];
+
+    $query = str_replace(array_keys($params), array_values($params), "
+        SELECT {$entity_revision_key}
+        FROM {$revision_table}
+        WHERE id = {$entity_revision->id()}
+        AND {$entity_revision_key} <> {$entity_revision->getRevisionId()}
+        ORDER BY {$entity_revision_key} DESC
+        LIMIT 1");
+
+    $revision_id = $database->query($query)->fetchCol();
+    var_dump($revision_id);
+    if (!empty($revision_id)) {
+      return $this->loadRevision($revision_id[0]);
+    }
+    return FALSE;
+  }
+
+}
