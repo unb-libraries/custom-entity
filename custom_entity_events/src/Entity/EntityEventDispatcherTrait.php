de76e02659ba8177464ef7cc52c67c3a114f982f
Patrick Schilf <pschilf@unb.ca>
2021-11-17 16:11:26 -0400

    Shortcut for dispatching events when saving entities

--- custom_entity_events/src/Entity/EntityEventDispatcherTrait.php
+++ custom_entity_events/src/Entity/EntityEventDispatcherTrait.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\custom_entity_events\Entity;
 
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\custom_entity_events\Event\EntityEvent;
 use Drupal\custom_entity_events\Event\EntityEvents;
 
@@ -28,6 +29,24 @@ trait EntityEventDispatcherTrait {
   }
 
   /**
+   * Dispatch events related to saving  the given entity.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity that has been or will be saved.
+   * @param bool $is_new
+   *   Whether the given entity is (or has been, before saving it) new.
+   */
+  protected function dispatchSave(EntityInterface $entity, bool $is_new) {
+    $this->dispatch(EntityEvents::SAVE, $entity);
+    if (!$is_new) {
+      $this->dispatch(EntityEvents::CREATE, $entity);
+    }
+    else {
+      $this->dispatch(EntityEvents::UPDATE, $entity);
+    }
+  }
+
+  /**
    * Dispatch an event of the given type.
    *
    * @param string $event_type
@@ -35,7 +54,7 @@ trait EntityEventDispatcherTrait {
    * @param \Drupal\Core\Entity\EntityInterface $entity
    *   The entity that is being saved.
    */
-  protected function dispatch($event_type, $entity) {
+  protected function dispatch(string $event_type, EntityInterface $entity) {
     if (($dispatcher = $this->eventDispatcher()) && in_array($event_type, EntityEvents::getEvents())) {
       $dispatcher->dispatch(new EntityEvent($entity), $event_type);
     }
