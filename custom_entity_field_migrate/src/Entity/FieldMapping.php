ad70894de68e3f224f47c01a8f5f880d2a625cf0
Patrick Schilf <pschilf@unb.ca>
2023-01-10 11:34:28 -0400

    Implement field mapping class

--- custom_entity_field_migrate/src/Entity/FieldMapping.php
+++ custom_entity_field_migrate/src/Entity/FieldMapping.php
@@ -8,38 +8,95 @@ namespace Drupal\custom_entity_field_migrate\Entity;
 class FieldMapping implements FieldMappingInterface {
 
   /**
+   * The source table name.
+   *
+   * @var string
+   */
+  protected $sourceTable;
+
+  /**
+   * The target table name.
+   *
+   * @var string
+   */
+  protected $targetTable;
+
+  /**
+   * The source table key.
+   *
+   * @var string|array
+   */
+  protected $sourceKey;
+
+  /**
+   * The target table key.
+   *
+   * @var string|array
+   */
+  protected $targetKey;
+
+  /**
+   * The source-column-to-target-column map.
+   *
+   * @var array.
+   */
+  protected $columnMap;
+
+  /**
+   * Create a new field mapping instance.
+   *
+   * @param string $source_table
+   *   A database table name.
+   * @param string $target_table
+   *   A database table name.
+   * @param mixed $source_key
+   *   One or multiple column name(s) of the given source table.
+   * @param mixed $target_key
+   *   One or multiple column name(s) of the given target table.
+   * @param array $column_map
+   *   An associate array assigning column names to other column names.
+   */
+  public function __construct(string $source_table, string $target_table, mixed $source_key, mixed $target_key, array $column_map) {
+    $this->sourceTable = $source_table;
+    $this->targetTable = $target_table;
+    $this->sourceKey = $source_key;
+    $this->targetKey = $target_key;
+    $this->columnMap = $column_map;
+  }
+
+  /**
    * {@inheritDoc}
    */
   public function getSourceTable() {
-    // TODO: Implement getSourceTable() method.
+    return $this->sourceTable;
   }
 
   /**
    * {@inheritDoc}
    */
   public function getTargetTable() {
-    // TODO: Implement getTargetTable() method.
+    return $this->targetTable;
   }
 
   /**
    * {@inheritDoc}
    */
   public function getSourceKey() {
-    // TODO: Implement getSourceKey() method.
+    return $this->sourceKey;
   }
 
   /**
    * {@inheritDoc}
    */
   public function getTargetKey() {
-    // TODO: Implement getTargetKey() method.
+    return $this->targetKey;
   }
 
   /**
    * {@inheritDoc}
    */
   public function getColumnMap() {
-    // TODO: Implement getColumnMap() method.
+    return $this->columnMap;
   }
 
 
