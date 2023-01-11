e70806eaebf6027e531a1e840af8246f1ba2a244
Patrick Schilf <pschilf@unb.ca>
2023-01-11 10:34:12 -0400

    Create field schema

---
+++ custom_entity_field_migrate/src/Entity/FieldSchema.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\custom_entity_field_migrate\Entity;
+
+/**
+ * Provides access an entity field's underlying database layout.
+ */
+class FieldSchema implements FieldSchemaInterface {
+
+  /**
+   * The database table names.
+   *
+   * @var array
+   */
+  protected $tables;
+
+  /**
+   * The mapping between field properties and database columns.
+   *
+   * @var array
+   */
+  protected $columns;
+
+  /**
+   * The database table primary keys.
+   *
+   * @var array
+   */
+  protected $keys;
+
+  /**
+   * @param string $tables
+   *   An array of database table names.
+   * @param string $columns
+   *   An array of the form FIELD_PROPERTY => DB_COLUMN_NAME.
+   * @param array $keys
+   *   An array of the form DB_TABLE_NAME => PRIMARY_KEY
+   */
+  public function __construct(array $tables, array $columns, array $keys) {
+    $this->tables = $tables;
+    $this->columns = $columns;
+    $this->keys = $keys;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getTables() {
+    return $this->tables;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getPropertyColumnMap() {
+    return $this->columns;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getKey(string $table) {
+    return $this->keys[$table];
+  }
+
+}
---
+++ custom_entity_field_migrate/src/Entity/FieldSchemaInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\custom_entity_field_migrate\Entity;
+
+/**
+ * Interface for field schema objects.
+ *
+ * A field schema provides insights into the part of the underlying database
+ * layout that is relevant to a given field.
+ */
+interface FieldSchemaInterface {
+
+  /**
+   * The database table names.
+   *
+   * @return array
+   *   An array of database table names.
+   */
+  public function getTables();
+
+  /**
+   * A map of field properties and database column names.
+   *
+   * @return array
+   *   An array keyed by field properties, assigning each a database column
+   *   name.
+   */
+  public function getPropertyColumnMap();
+
+  /**
+   * Get the name of a database column that acts as key for the given table.
+   *
+   * @param string $table
+   *   A database table name.
+   *
+   * @return string
+   *   A database column name.
+   */
+  public function getKey(string $table);
+
+}
